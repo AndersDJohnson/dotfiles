2eea2e1ab1d5226b449867dfa2d04a9471f2f021
Anders D. Johnson <anders.d.johnson.developer@gmail.com>
2015-06-26 12:49:59 -0500

    Link bash_profile in install

--- install.sh
+++ install.sh
@@ -10,14 +10,12 @@ git submodule update --init
 
 ln -sf $BASE/zshrc ~/.zshrc
 ln -sf $BASE/oh-my-zsh ~/.oh-my-zsh
-
 ln -sf $BASE/bashrc ~/.bashrc
+ln -sf $BASE/bash_profile ~/.bash_profile
 ln -sf $BASE/bash_aliases ~/.bash_aliases
 ln -sf $BASE/commonshrc ~/.commonshrc
 ln -sf $BASE/inputrc ~/.inputrc
-
 ln -sf $BASE/bcrc ~/.bcrc
-
 ln -sf $BASE/tmux.conf ~/.tmux.conf
 
 # install gitconfig

ecadad5581591ed6241eabf5ca22f5d63e273f61
Anders D. Johnson <anders.d.johnson.developer@gmail.com>
2015-07-07 22:06:53 -0500

    Update mac install.

--- install-mac.sh
+++ install-mac.sh
@@ -1,4 +1,23 @@
+BREW_APPS=(\
+coreutils \
+caskroom/cask/brew-cask \
+hub \
+)
+CASK_APPS=(\
+google-chrome \
+sublime-text \
+)
 
-brew install coreutils
+for BREW_APP in "${BREW_APPS[@]}"; do
+  echo "Installing \"$BREW_APP\"..."
+  brew install --force $BREW_APP
+  echo "...installed \"$BREW_APP\"."
+done
+
+for CASK_APP in "${CASK_APPS[@]}"; do
+  echo "Installing \"$CASK_APP\"..."
+  brew cask install --force $CASK_APP
+  echo "...installed \"$CASK_APP\"."
+done
 
 

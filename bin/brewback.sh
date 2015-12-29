fe8130b87fdadcbcba37b634032bb8058e73f137
Anders D. Johnson <anders.d.johnson.developer@gmail.com>
2015-12-28 21:17:26 -0600

    Add brew backup script.

---
+++ bin/brewback.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd ~/.dotfiles
+brew list > brew.txt
+brew cask list > brew-cask.txt
+git reset
+git add brew.txt brew-cask.txt
+git commit -m "Update brew and cask lists."
+git push
+
